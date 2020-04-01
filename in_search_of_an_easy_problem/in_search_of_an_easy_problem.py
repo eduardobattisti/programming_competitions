b0f99426d6aa47a72b75a060d0a3675f3d7c7fc7
eduardobattisti <eduardobattisti@protonmail.com>
2020-04-01 12:23:27 -0300

    solution + code explain

---
+++ in_search_of_an_easy_problem/in_search_of_an_easy_problem.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# The first input is initialized, and must receive only an integer
+# The second part of the code makes the user input a list of numbers
+# If this list has the number 1 it will return the second index (HARD)
+# of the tuple, which is hard, else it will return EASY.
+
+int(input()); print(('EASY', 'HARD')['1'in input()])
+
